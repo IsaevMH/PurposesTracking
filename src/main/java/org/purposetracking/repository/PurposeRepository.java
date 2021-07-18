c703f86ab58e33692756fd2c6f8cbef3f332a43b
iwork <iwork30rus@gmail.com>
2021-07-19 00:04:09 +0400

    Empty commit

--- src/main/java/org/purposetracking/repository/PurposeRepository.java
+++
@@ -1,9 +0,0 @@
-package org.purposetracking.repository;
-
-import org.purposetracking.model.Purpose;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PurposeRepository extends JpaRepository<Purpose, Long> {
-}
--- src/main/java/org/purposetracking/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package org.purposetracking.repository;
-
-import org.purposetracking.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
